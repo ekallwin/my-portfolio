4bc58cb0f1758428c6cfd50734fb193da4e14b29
ekallwin <ekallwin@gmail.com>
2025-08-07 20:27:20 +0530

    Added Error

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,34 @@ import ScrollToTop from './ScrollToTop';
 import Home from "./Home";
 import Projects from "./Components/Projects/project";
 import Achievements from "./Components/Achievements/achievements";
-
+import Error from './Components/Error/Error'
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable={false}
-        pauseOnHover={false}
-        theme="colored"
-        transition={Bounce}
-      />
-      <ScrollToTop />
-      <Routes>
-        <Route path="*" element={<Navigate to="/" replace />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/achievements" element={<Achievements />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable={false}
+          pauseOnHover={false}
+          theme="colored"
+          transition={Bounce}
+        />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/achievements" element={<Achievements />} />
+          <Route path="/error" element={<Error />} />
+          <Route path="*" element={<Navigate to="/error" replace />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Error.css';
+
+const Error = () => {
+    const navigate = useNavigate();
+
+    const handleGoHome = () => {
+        navigate('/');
+    };
+
+    return (
+        <div className="not-found-container">
+            <div className="not-found-content">
+                <h4 className='error-head'>Sorry, this page isn't available</h4>
+                <br />
+                <p className='error-txt'>The link you followed may be broken, or the page may have been removed</p>
+                <button className="btn btn-primary" onClick={handleGoHome}>
+                    Go back to Home
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
