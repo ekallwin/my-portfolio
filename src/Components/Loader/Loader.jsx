e816015c4c780b8a55f9836d2913c4b348397e8b
ekallwin <ekallwin@gmail.com>
2025-08-23 11:15:38 +0530

    Added Loader

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './loader.css'
+function Loader() {
+    return (
+        <>
+            <div className="loader-wrapper">
+                <svg className="spinner" viewBox="25 25 50 50">
+                    <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth="4" />
+                </svg>
+            </div>
+        </>
+    )
+}
+
+export default Loader
