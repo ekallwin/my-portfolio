6585afb1572038b1583604fdb2e654ad35fe26be
ekallwin <ekallwin@gmail.com>
2025-08-31 15:15:24 +0530

    Minor Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "./App.css";
 import { Bounce, ToastContainer } from "react-toastify";
